df883bc35ee8188fd0fce4bcae4d7be69a2b114c
Emil Taylor Bye <emiltayl@dmlab10.idi.ntnu.no>
2013-01-30 10:51:51 +0100

    Store words, now with offsets!

--- oeving1.s
+++ oeving1.s
@@ -13,32 +13,28 @@ SR_GM =   16  /* statusregisterflag "GM" er bit 16 */
 /* text-segment */
 /* all programkoden må plasseres i dette segmentet */
 
+/*
+ * r0: pioc
+ * r1: 0xff
+ * r2: piob
+ * r4: current light position
+*/
 .text
 
 .globl  _start
 _start: /* programutføring vil starte her */
-    lddpc r0, per_pioc_peker
-    lddpc r2, oer_pioc_peker
-    lddpc r3, sodr_pioc_peker
-    lddpc r5, codr_pioc_peker
-    lddpc r6, per_piob_peker
-    lddpc r7, puer_piob_peker
-    lddpc r8, pdsr_piob_peker
+    lddpc r0, pioc_offset
+    lddpc r2, piob_offset
     mov r4, 0x08 /* 4 holds the paddle */
     mov r1, 0xff
-    st.w r0, r1 /* Set PER for all LEDs */
-    st.w r2, r1 /* Set OER for all LEDs */
-    st.w r6, r1 /* Set PER for all buttons */
-    st.w r7, r1 /* Set PUER for all buttons */
-    /* st.w r3, r1  Enable the LEDs in R1 */
-    st.w r5, r1 /* Disable the LEDs in R1 */
-
-    /* eksempel på lesing fra minneområdet "eksempelvariabel"
-    lddpc r0, led_pioc_peker  /* last peker til r0
-    ld.w r1, r0  /* last fra minnet til r1 */
+    st.w r0[AVR32_PIO_PER], r1 /* Set PER for all LEDs */
+    st.w r0[AVR32_PIO_OER], r1 /* Set OER for all LEDs */
+    st.w r2[AVR32_PIO_PER], r1 /* Set PER for all buttons */
+    st.w r2[AVR32_PIO_PUER], r1 /* Set PUER for all buttons */
+    st.w r0[AVR32_PIO_OER], r1 /* Disable the LEDs in R1 */
 
 loop:   /* evig løkke */
-    st.w r5, r1 /* turns all LEDs off*/
+    st.w r0[AVR32_PIO_CODR], r1 /* turns all LEDs off*/
     rcall read_buttons
     cp.w r12, 0x1
     brne else_if /* branches to else_if if button 0 was not pushed */
@@ -51,12 +47,12 @@ else_if:
     rcall move_paddle_left
 
 end_if:
-    st.w r3, r4
+    st.w r0[AVR32_PIO_SODR], r4 /* Enable the LED that should be enabled */
     rjmp loop
 
 
 read_buttons:
-    ld.w r12, r8
+    ld.w r12, r2[AVR32_PIO_PDSR]
     eor r12, r1 /* == not R12, for the first 8 bits */
     and r12, r1 /* to ensure the first 8 bits remain untouched while the last 8 are set to zero */
     ret r12
@@ -87,24 +83,13 @@ move_paddle_left_reset:
 move_paddle_left_end:
     ret r12
 
-oer_pioc_peker:
-    .int AVR32_PIOC|AVR32_PIO_OER
-per_pioc_peker:
-    .int AVR32_PIOC|AVR32_PIO_PER
-sodr_pioc_peker: /* Turn LEDs on */
-    .int AVR32_PIOC|AVR32_PIO_SODR
-codr_pioc_peker: /* Turn LEDs off */
-    .int AVR32_PIOC|AVR32_PIO_CODR
-per_piob_peker:
-    .int AVR32_PIOB|AVR32_PIO_PER
-puer_piob_peker: /* Pull-up resistors */
-    .int AVR32_PIOB|AVR32_PIO_PUER
-pdsr_piob_peker: /* To read button status */
-    .int AVR32_PIOB|AVR32_PIO_PDSR
+pioc_offset:
+    .int AVR32_PIOC
+piob_offset:
+    .int AVR32_PIOB
 
 /*****************************************************************************/
 /* data-segment */
 /* alle dataområder som skal kunne skrives til må plasseres her */
 
 .data
-
