e14133af864bdc2b3a21a1f9a4c8f3ad738b296f
Odd Magnus Trondrud <trondrud@stud.ntnu.no>
2013-02-25 17:12:25 +0100

    fixed indentation

--- 2/oeving2.c
+++ 2/oeving2.c
@@ -15,40 +15,42 @@ volatile avr32_pio_t *pioc = &AVR32_PIOC;
 volatile avr32_smc_t *sm = &AVR32_SMC;
 
 int main(int argc, char *argv[]) {
-  init_hardware();
+    init_hardware();
 
-  leds_off(0xff);
-  leds_on(0xaa);
+    leds_off(0xff);
+    leds_on(0xaa);
 
-  while(1);
-  return 0;
+
+    while(1) {
+    }
+    return 0;
 }
 
 /* funksjon for å initialisere maskinvaren, må utvides */
 void init_hardware(void) {
-  init_intc();
-  leds_init(pioc, 0xff);
-  init_buttons();
-  init_audio();
+    init_intc();
+    leds_init(pioc, 0xff);
+    init_buttons();
+    init_audio();
 }
 
 void init_intc(void) {
-  set_interrupts_base((void *)AVR32_INTC_ADDRESS);
-  init_interrupts();
+    set_interrupts_base((void *)AVR32_INTC_ADDRESS);
+    init_interrupts();
 }
 
 void init_buttons(void) {
-  register_interrupt(button_isr, AVR32_PIOB_IRQ / 32, AVR32_PIOB_IRQ % 32, BUTTONS_INT_LEVEL);
-  /* (...) */
-  /* Enable I/O pins for them buttans */
-  piob->per = 0xff;
-  /* Enable pull-up resistors */
-  piob->puer = 0xff;
+    register_interrupt(button_isr, AVR32_PIOB_IRQ / 32, AVR32_PIOB_IRQ % 32, BUTTONS_INT_LEVEL);
+    /* (...) */
+    /* Enable I/O pins for them buttans */
+    piob->per = 0xff;
+    /* Enable pull-up resistors */
+    piob->puer = 0xff;
 }
 
 void init_audio(void) {
-  register_interrupt(abdac_isr, AVR32_ABDAC_IRQ / 32, AVR32_ABDAC_IRQ % 32, ABDAC_INT_LEVEL);
-  /* (...) */
+    register_interrupt(abdac_isr, AVR32_ABDAC_IRQ / 32, AVR32_ABDAC_IRQ % 32, ABDAC_INT_LEVEL);
+    /* (...) */
 }
 
 void button_isr(void) {
