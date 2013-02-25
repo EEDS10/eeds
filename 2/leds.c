b6036a61d0ee8820aa596e64e74f176d562b1894
Odd Magnus Trondrud <trondrud@stud.ntnu.no>
2013-02-25 16:46:07 +0100

    capitalization and typos

--- 2/leds.c
+++ 2/leds.c
@@ -5,24 +5,24 @@ static volatile avr32_pio_t *pio;
 
 void leds_init(volatile avr32_pio_t* _pio, int bitfield) {
 
-    /* sore a reference to the PIO which has the leds */
+    /* Store a reference to the PIO which has the leds */
     pio = _pio;
 
-    /* enable IO pins */
+    /* Enable IO pins */
     pio->per = bitfield;
 
-    /* set pins to act as output */
+    /* Set pins to act as output */
     pio->oer = bitfield;
 }
 
 void leds_on(int bitfield){
     
-    /* turn on leds */
+    /* Turn on leds */
     pio->sodr = bitfield;
 }
 
 void leds_off(int bitfield){
 
-    /* turn off leds */
+    /* Turn off leds */
     pio->codr = bitfield;
 }
