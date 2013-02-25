6189b16ba0c26b243dd1570d030b8b68810970fa
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2013-02-25 19:45:34 +0100

    Added routine to read button state

--- 2/buttons.c
+++ 2/buttons.c
@@ -3,12 +3,22 @@
 #include "buttons.h"
 #include "oeving2.h"
 
-void buttons_init(volatile avr32_pio_t *pio, int bitmask, void *handler, int IRQ) {
-    register_interrupt((__int_handler)(handler), IRQ / 32, IRQ % 32, BUTTONS_INT_LEVEL);
+static volatile avr32_pio_t *pio;
+
+void buttons_init(volatile avr32_pio_t *_pio, int bitmask, void *handler, int IRQ) {
+
+    /* Store a reference to the PIO which has the leds */
+    pio = _pio;
 
     /* Enable I/O pins for them buttans */
     pio->per = bitmask;
 
     /* Enable pull-up resistors */
     pio->puer = bitmask;
+
+    register_interrupt((__int_handler)(handler), IRQ / 32, IRQ % 32, BUTTONS_INT_LEVEL);
+}
+
+void buttons_read(){
+    return ~pio->pdsr;
 }
