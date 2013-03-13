88e5eae8df7e04c99ce1b811df11d65eb8b9393b
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2013-03-13 23:39:17 +0100

    Added some comments to the code

--- 2/oeving2.c
+++ 2/oeving2.c
@@ -17,61 +17,76 @@
 #include "modfiles.h"
 #include "synth.h"
 
+#define SAMPLE_RATE (46875/2)
+
 #define SELECTION_NONE 7
 #define N_MODS 4
 #define N_SOUNDS 3
 
+/* hardware handles */
 volatile avr32_abdac_t *dac = &AVR32_ABDAC;
 volatile avr32_pio_t *piob = &AVR32_PIOB;
 volatile avr32_pio_t *pioc = &AVR32_PIOC;
 volatile avr32_pm_t *pm = &AVR32_PM;
 
+/* variables used for MOD playback */
 MOD* MODS[N_MODS];
 MOD_Player* player;
+int mod_ticks = 0;
 
-int current_selection = SELECTION_NONE;
-int ticks = 0;
-
+/* variables used for sound effect playback */
 sound_t SOUNDS[N_SOUNDS];
 playback_t SOUND_PLAYBACKS[N_SOUNDS];
 playback_t *current_synth_sound = NULL;
 
-#define SAMPLE_RATE (46875/2)
+/* variables used for keeping track of the user interface state */
+int current_selection = SELECTION_NONE;
+
 
 int main(int argc, char *argv[]) {
 
+    /* perpare sound effects for playback */
     prepare_playback(&SOUND_PLAYBACKS[0], get_sound(&SOUNDS[0], SQUARE, 500, 500, 1250, 0, 250, 20), SAMPLE_RATE, 20000);
     prepare_playback(&SOUND_PLAYBACKS[1], get_sound(&SOUNDS[1], SAWTOOTH, 2200, 100, 100, 0, 500, 70), SAMPLE_RATE, 20000);
     prepare_playback(&SOUND_PLAYBACKS[2], get_sound(&SOUNDS[2], NOISE, 1, 0, 1000, 0, 0, 0), SAMPLE_RATE, 20000);
 
+    /* prepare MOD files for playback */
     MODS[0] = MOD_load(MODFILES_BACONGRYTOR_MOD);
     /*
     MODS[1] = MOD_load(MODFILES_HOFFMAN___DROP_THE_PANIC__TWEAKED__MOD);
     MODS[2] = MOD_load(MODFILES_BOESENDORFER_P_S_S_MOD);
     MODS[3] = MOD_load(MODFILES_TUULENVIRE_MOD);
     */
-
     player = MOD_Player_create(SAMPLE_RATE);
 
+    /* Activate leds, buttons, audio. This is done as late as
+     * possible in the setup phase to avoid premature interrupts. */
     init_hardware();
 
     leds_off(0xff);
 
-
+    /* main loop */
     while(1){
-        while(ticks > 0){
+
+        /* if we need to logical advance the mod player, do so */
+        while(mod_ticks > 0){
             if(player->mod != NULL){
-                ticks-=16;
+                mod_ticks-=16;
                 MOD_Player_step(player, 1000000/SAMPLE_RATE*16);
             }
         }
+
+        /* if we are done playing a sound effect, mark it as done */
         if(current_synth_sound != NULL && playback_finished(current_synth_sound)){
             select(SELECTION_NONE);
         }
     }
 
-    return 0;
+    /* Return success. This is ironic, because if the program ever
+     * gets this far, something must have gone wrong :D */
+    return EXIT_SUCCESS;
 }
+
 
 /* sets up the hardware */
 void init_hardware(void) {
@@ -81,6 +96,8 @@ void init_hardware(void) {
     init_intc();
 }
 
+
+/* sets up interrupts */
 void init_intc(void) {
     /* as specified on page 45 of the compendium,
      * the arguments of set_interrupts_base will always be
@@ -93,7 +110,10 @@ void init_intc(void) {
 }
 
 
+/* selects a MOD file or sound effect to play */
 void select(int selection){
+
+    /* set the correct sound to play */
     if(SELECTION_NONE == selection){
         MOD_Player_set_mod(player, NULL);
         current_synth_sound = NULL;
@@ -106,8 +126,10 @@ void select(int selection){
         reset_playback(current_synth_sound);
     }
 
+    /* update state variables */
     current_selection = selection;
 
+    /* update the user interface */
     leds_off(0xff);
     if(current_selection != SELECTION_NONE){
         leds_on(1<<current_selection);
@@ -115,6 +137,7 @@ void select(int selection){
 }
 
 
+/* button interrupt subroutine */
 void button_isr(void) {
     piob->isr;
 
@@ -122,6 +145,7 @@ void button_isr(void) {
 
     if(!buttons) return;
 
+    /* use the leftmost pressed button */
     int selected = 0;
     while(buttons = buttons>>1){
         selected++;
@@ -131,13 +155,14 @@ void button_isr(void) {
 }
 
 
+/* abdac interrupt subroutine */
 void abdac_isr(void) {
     int32_t out = 0;
 
     /* Play mod files */
     if(player->mod != NULL){
         out += MOD_Player_play(player);
-        ticks++;
+        mod_ticks++;
     }
 
     /* Play sound effects */
@@ -145,6 +170,7 @@ void abdac_isr(void) {
         out += next_sample(current_synth_sound);
     }
 
+    /* give the output to the hardware */
     dac->SDR.channel0 = out;
     dac->SDR.channel1 = out;
 }
