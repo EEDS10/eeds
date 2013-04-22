f9b5d0b2d38687b77703af59a9a90a270c44f4f2
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2013-04-22 16:46:20 +0200

    Started reading songs from dir

--- 3/game/src/MainMenuState.c
+++ 3/game/src/MainMenuState.c
@@ -1,5 +1,6 @@
 #include <allegro.h>
 #include <stdio.h>
+#include <dirent.h>
 #include "State.h"
 #include "SMSong.h"
 #include "Font.h"
@@ -9,11 +10,40 @@ extern State* GameState;
 Font* font_large;
 Font* font_small;
 
+SMSong** songs;
+int n_songs;
+
 bitmap_t* test;
 
 static void state_init(){
     font_large = Font_load("res/fonts/font_large.bmp", 28, 36);
     font_small = Font_load("res/fonts/font_small.bmp", 20, 24);
+
+    DIR *dp;
+    struct dirent *ep;
+
+    n_songs = 0;
+    dp = opendir("res/Songs/");
+    while(ep = readdir (dp)){
+        n_songs++;
+    }
+    closedir(dp);
+    songs = (SMSong**) malloc(sizeof(SMSong*) * n_songs);
+    dp = opendir("res/Songs/");
+    char filename[256];
+    strcpy(filename, "res/Songs/");
+    for(int i=0;i<n_songs;i++){
+        ep = readdir(dp);
+        if(ep->d_name[0] == '.') continue;
+        strcpy(filename+10, ep->d_name);
+        strcpy(filename+10 + strlen(ep->d_name), "/");
+        strcpy(filename+10 + strlen(ep->d_name) + 1, "/");
+        strcpy(filename+10 + strlen(ep->d_name) + 1, ep->d_name);
+        strcpy(filename+10 + 2*strlen(ep->d_name) + 1, ".sm");
+        printf("filename: %s\n", filename);
+        songs[i] = SMSong_load(filename); 
+    }
+    closedir(dp);
 
     FILE* fp = fopen("res/test.bmp", "rw");
     test = eeds_load_bmp(fp);
