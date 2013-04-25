8b8069991ec0670b8404a6f3eae381c53525d3bc
Emil Taylor Bye <emil@wodinaz.com>
2013-04-25 02:53:09 +0200

    Reading keys on the STK1000 could very well be like this

--- 3/game/src/allegro_shim.c
+++ 3/game/src/allegro_shim.c
@@ -2,8 +2,22 @@
 #define ALLEGRO_SHIM_H
 #ifdef NO_ALLEGRO
 
+#include <stdio.h>
+
 #include "allegro_shim.h"
 
+void install_keyboard() {
+    keyboard = fopen("/dev/buttons", "rb");
+}
+
+void clear_keybuf() {
+    int keystates = fgetc(keyboard);
+
+    key[0] = !!(keystates & 128);
+    key[1] = !!(keystates & 64);
+    key[2] = !!(keystates & 32);
+    key[3] = !!(keystates & 16);
+}
 
 #endif
 #endif
