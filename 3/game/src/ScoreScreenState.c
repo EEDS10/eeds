119e86f34b75e8ad0ac2632ccccd6d102caa9152
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2013-04-25 23:22:06 +0200

    Tweaked songs, fixed score screen bug

--- 3/game/src/ScoreScreenState.c
+++ 3/game/src/ScoreScreenState.c
@@ -12,7 +12,6 @@ extern State* MainMenuState;
 extern int score_perfect;
 extern int score_great;
 extern int score_OK;
-extern int score_OK;
 extern int score_combo;
 extern int score_greatest_combo;
 
@@ -42,13 +41,13 @@ static void state_render(bitmap_t* buffer){
     sprintf(message, "PERFECT!: %i", score_perfect);
     Font_render(font_small, buffer, message, 5, 15, 9);
 
-    sprintf(message, "Great!: %i", score_perfect);
+    sprintf(message, "Great!: %i", score_great);
     Font_render(font_small, buffer, message, 5, 45, 9);
 
-    sprintf(message, "O.K.!: %i", score_perfect);
+    sprintf(message, "O.K.: %i", score_OK);
     Font_render(font_small, buffer, message, 5, 75, 9);
 
-    sprintf(message, "Best combo: %i", score_perfect);
+    sprintf(message, "Best combo: %i", score_greatest_combo);
     Font_render(font_large, buffer, message, 5, 135, 11);
     Font_render(font_large, buffer, "GREAT, YOU ROCK!", 5, 185, 11);
 }
